aefa9c777fa1437bb8a6e688efff7f49f118cd98
shenjinqing <sjq_nuaa@163.com>
2020-06-28 14:13:31 +0800

    添加Nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link class="item" to="/money">
       <Icon name="money" />记账
-    </router-link>|
-    <router-link to="/labels">
+    </router-link>
+    <router-link class="item" to="/labels">
       <Icon name="label" />标签
-    </router-link>|
-    <router-link to="/statistics">
+    </router-link>
+    <router-link class="item" to="/statistics">
       <Icon name="statistics" />统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -20,7 +20,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  border: 1px solid red;
+// 导航栏样式
+nav {
+  display: flex;
+  justify-content: space-around;
+  box-shadow: 0 0 3px rgba(0, 0, 0.2);
+  font-size: 12px;
+  // router-link的样式
+  > .item {
+    padding: 2px 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    // 图标样式
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
 }
 </style>
