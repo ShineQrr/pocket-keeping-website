5176f05466ca6298b6ab8cd7eed241d47cea2bd3
shenjinqing <sjq_nuaa@163.com>
2020-07-03 23:42:20 +0800

    监听四个组件的输出

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -30,6 +30,7 @@ export default class Tags extends Vue {
     } else {
       this.selectedTags.push(tag);
     }
+    this.$emit("update:selected", this.selectedTags);
   }
   addTag() {
     const tagName = window.prompt("请输入标签名");
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,37 +1,54 @@
 <template>
   <div>
     <Layout class-prefix="layout">
+      {{ record }}
       <!-- 顶部标签 -->
-      <Tags :data-source.sync="tags"></Tags>
+      <Tags :data-source.sync="tags" @update:selected="onUpdateTags"></Tags>
       <!-- 备注栏 -->
-      <Notes></Notes>
+      <Notes @update:value="onUpdateNotes"></Notes>
       <!-- 支出/收入 -->
-      <Types></Types>
+      <Types @update:value="onUpdateType"></Types>
       <!-- 数字面板 -->
-      <NumberPad></NumberPad>
+      <NumberPad @update:value="onUpdateAmount"></NumberPad>
     </Layout>
   </div>
 </template>
 
 <script lang="ts">
+import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Tags from "@/components/Money/Tags.vue";
 import Notes from "@/components/Money/Notes.vue";
-export default {
-  name: "Money",
-  components: {
-    NumberPad,
-    Types,
-    Tags,
-    Notes
-  },
-  data() {
-    return {
-      tags: ["衣", "食", "住", "行", "玩"]
-    };
-  }
+import { Component } from "vue-property-decorator";
+
+type Record = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
 };
+
+@Component({
+  components: { NumberPad, Types, Tags, Notes }
+})
+export default class Money extends Vue {
+  tags = ["衣", "食", "住", "行", "玩"];
+  record: Record = { tags: [], notes: "", type: "-", amount: 0 };
+
+  onUpdateTags(tags: string[]) {
+    this.record.tags = tags;
+  }
+  onUpdateNotes(notes: string) {
+    this.record.notes = notes;
+  }
+  onUpdateType(type: string) {
+    this.record.type = type;
+  }
+  onUpdateAmount(value: string) {
+    this.record.amount = parseFloat(value);
+  }
+}
 </script>
 <style lang="scss">
 .layout-content {
