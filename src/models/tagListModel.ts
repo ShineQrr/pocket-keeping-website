6c8db90d9ad4aec3e3cf1394143eb27b3fba4ae2
shenjinqing <sjq_nuaa@163.com>
2020-07-09 23:19:16 +0800

    完成EditLabel.vue的编辑和删除标签功能

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -8,6 +8,8 @@ type TagListModel = {
     fetch: () => Tag[];
     add: (name: string) => { code: number; message: string };// 1 表示成功 0表示name重复
     save: () => void;
+    update: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
+    remove: (id: string) => boolean;
 }
 const tagListModel: TagListModel = {
     data: [],
@@ -24,6 +26,32 @@ const tagListModel: TagListModel = {
         this.data.push({ id: name, name: name })
         this.save()
         return { code: 1, message: 'success' };
+    },
+    // 更新数据:将对应的id 更新成对应的name
+    update(id, name) {
+        const idList = this.data.map(item => item.id);
+        if (idList.includes(id)) {
+            const names = this.data.map(item => item.name);
+            if (names.includes(name)) {
+                return 'duplicated';
+            } else {
+                const tag = this.data.filter(item => item.id === id)[0];
+                tag.name = name;
+                // 保证id也一致
+                // tag.id = name;
+                this.save();
+                return 'success';
+            }
+        } else {
+            return 'not found';
+        }
+    },
+    // 删除数据
+    remove(id: string) {
+        const remainData = this.data.filter(item => item.id !== id)
+        this.data = remainData;
+        this.save();
+        return true;
     },
     // 保存数据
     save() {
