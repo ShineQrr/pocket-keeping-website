884968e0651a5d35cd7737aa84ccaceb7eb3a82b
shenjinqing <sjq_nuaa@163.com>
2020-07-15 10:14:26 +0800

    删除models,内容都引入store

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,7 +1,24 @@
-import recordListModel from '@/models/recordListModel';
+import clone from '@/lib/clone';
 
-export default {
-    // record store
-    recordList: recordListModel.fetch(),
-    createRecord: (record: RecordItem) => recordListModel.createItem(record),
-} 
+const LOCAL_STORAGE_KEY_NAME = 'recordList';
+
+const recordStore = {
+    recordList: [] as RecordItem[],
+    fetchRecords() {
+        this.recordList = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY_NAME) || '[]') as RecordItem[];
+        return this.recordList;
+    },
+    saveRecords() {
+        window.localStorage.setItem(LOCAL_STORAGE_KEY_NAME, JSON.stringify(this.recordList));
+    },
+    createRecord(record: RecordItem) {
+        const recordCopy: RecordItem = clone(record);
+        recordCopy.createdAt = new Date();
+        this.recordList && this.recordList.push(recordCopy);
+        recordStore.saveRecords();
+    },
+};
+
+recordStore.fetchRecords();
+
+export default recordStore;
