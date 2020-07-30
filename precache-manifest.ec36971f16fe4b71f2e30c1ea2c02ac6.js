dbb0ac4b9165605babe3541ff47773d5527399dd
shenjinqing <sjq_nuaa@163.com>
2020-07-30 11:39:49 +0800

    deploy

---
+++ precache-manifest.ec36971f16fe4b71f2e30c1ea2c02ac6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a093e5aab0df375c06",
+    "url": "/pocket-keeping-website/css/app.f08d3baa.css"
+  },
+  {
+    "revision": "ff522704f0ee588d7f4a",
+    "url": "/pocket-keeping-website/css/chunk-vendors.0fc8e7c6.css"
+  },
+  {
+    "revision": "4601ef6ccabdd328a935c2d57f4b914b",
+    "url": "/pocket-keeping-website/index.html"
+  },
+  {
+    "revision": "82a093e5aab0df375c06",
+    "url": "/pocket-keeping-website/js/app.e2441088.js"
+  },
+  {
+    "revision": "ff522704f0ee588d7f4a",
+    "url": "/pocket-keeping-website/js/chunk-vendors.fbecd3ee.js"
+  },
+  {
+    "revision": "620113bda306167274054f52a15e75fe",
+    "url": "/pocket-keeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pocket-keeping-website/robots.txt"
+  }
+]);
